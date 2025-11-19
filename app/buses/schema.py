a5b6970b49cc998db350fb83ff4de52b2051b5ee
chrstndn <christine.diane_cuenca@questronix.com.ph>
2025-11-19 15:39:21 +0800

    feat: updated bus get, post, put endpoints

--- app/buses/schema.py
+++ app/buses/schema.py
@@ -10,8 +10,11 @@ class Location(BaseModel):
 
 class BusInfo(BaseModel):
     plate_number: str
+    bus_name: str
+    bus_number: str
     priority_seat: int
     capacity: int
+    origin: str
     destination: str
     status: Optional[Literal["available", "active", "offline"]] = "available"
     current_location: Optional[Location] = None
@@ -22,8 +25,11 @@ class BusInfo(BaseModel):
 class BusInfoResponse(BaseModel):
     id: str
     plate_number: str
+    bus_name: str
+    bus_number: str
     priority_seat: int
     capacity: int
+    origin: str
     destination: str
     status: Optional[str] = None
     current_location: Optional[Location] = None
@@ -35,8 +41,11 @@ class BusInfoResponse(BaseModel):
 
 class BusInfoUpdate(BaseModel):
     plate_number: Optional[str] = None
+    bus_name: Optional[str] = None
+    bus_number: Optional[str] = None
     priority_seat: Optional[int] = None
     capacity: Optional[int] = None
+    origin: Optional[str] = None
     destination: Optional[str] = None
     status: Optional[Literal["available", "active", "offline"]] = None
     current_location: Optional[Location] = None
